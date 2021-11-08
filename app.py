523c21d1ad50a98874048ed9f646aac0d51adcea
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-11-08 20:38:15 -0300

    tipando as rotas e criando rota de post

--- app.py
+++ app.py
@@ -1,21 +1,42 @@
 from flask import Flask, render_template
+from flask_restx import Api, Resource
+
 
 app = Flask(__name__, static_folder='./templates')
+api = Api(app, title="minha api", description="description for api")
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return {'hi': 'yeah'}
-    
 
-@app.route('/dog/<string:raca>')
+@app.route('/dog/<string:raca>', methods=['GET'])
 def dog(raca: str):
     return f'<h1> a raca do dog eh {raca} <h1>'
 
 
-
-@app.route('/person/<string:name>')
+@app.route('/person/<string:name>', methods=['GET'])
 def get_person(name: str):
     return render_template('hello.html', name= name)
+
+
+
+# !!!!!!!!!!!!!!!!API !!!!!!!!!!!!!!!
+
+@api.route('/doc/', methods=['POST'])
+class HelloApi(Resource):
+    def get(self):
+        return {'hi': 'yeah'},200
+    def post(self):
+        return {'hi': 'yeah'},201
+    def put(self):
+        return {'hi': 'yeah'},404
+    def delete(self):
+        return {'hi': 'yeah'},403
+    def patch(self):
+        return {'hi': 'yeah'},500
+
+
+
 
 
 
