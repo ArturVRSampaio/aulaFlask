ac4f21b4e846edc0263f3d7a3bb57be740e94d11
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-11-08 20:24:03 -0300

    carregando template html

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./templates')
 
 @app.route('/')
 def hello_world():
@@ -12,6 +12,12 @@ def dog(raca: str):
     return f'<h1> a raca do dog eh {raca} <h1>'
 
 
+
+@app.route('/person/<string:name>')
+def get_person(name: str):
+    return render_template('hello.html', name= name)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
